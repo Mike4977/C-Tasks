60816f170fa7696d837fd08ce0f47a8f88c4b653
mike77 <weilliss@mail.ru>
2023-02-22 00:27:07 +0600

    проверка на натуральность

--- task28/Program.cs
+++ task28/Program.cs
@@ -7,12 +7,16 @@
 
 Console.WriteLine("Введите натуральное число");
 int num = Convert.ToInt32(Console.ReadLine());
-int factorial = Factorial(num);
-Console.WriteLine($"Произведение чисел от 1 до {num} = {factorial}");
+
+if (num > 0) 
+{
+    int factorial = Factorial(num);
+    Console.WriteLine($"Произведение чисел от 1 до {num} = {factorial}");
+}
+else Console.WriteLine("Введено не натуральное число");
 
 int Factorial(int number)
 {
-    
     int res = 1;
     for (int i = 1; i <= number; i++)
     {
