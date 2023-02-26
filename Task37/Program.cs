353d2fbae35b170df12722fd6d3d8edb3cab7d84
mike77 <weilliss@mail.ru>
2023-02-26 12:13:05 +0600

    add task37

---
+++ Task37/Program.cs
@@ -0,0 +1,48 @@
+﻿// Задача 37: Найдите произведение пар чисел в одномерном массиве.
+// Парой считаем первый и последний элемент, второй и предпоследний
+// и т.д. Результат запишите в новом массиве.
+// [1 2 3 4 5] -> 5 8 3
+// [6 7 3 6] -> 36 21
+
+int[] FillArrayRndInt(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] =rnd.Next(min, max + 1);
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr)
+{
+     Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write($"{arr[i]}, ");
+		else Console.Write(arr[i]);
+    }
+    Console.Write("]");
+}
+
+int[] NewArray(int[] arr)
+{
+    int newSize = arr.Length / 2;
+    if (arr.Length % 2 != 0) newSize += 1;
+    
+    int[] newArr = new int[newSize];
+    for (int i = 0; i < newSize; i++)
+    {
+        newArr[i] = arr[i] * arr[arr.Length - 1 - i];
+    }
+    if (arr.Length % 2 != 0)
+        newArr[newArr.Length - 1] = arr[newSize - 1];
+    return newArr;
+}
+int[] array = FillArrayRndInt(11, 1, 9);
+int[] newArray = NewArray(array);
+PrintArray(array);
+Console.WriteLine();
+
+PrintArray(newArray);
