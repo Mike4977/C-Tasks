ec3bc0146aa58b8cd8d1700fcf6e5c90dd086402
mike77 <weilliss@mail.ru>
2023-03-01 00:27:55 +0600

    add task 42

---
+++ task40/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 40: Напишите программу, которая принимает на вход три
+// числа и проверяет, может ли существовать треугольник с сторонами
+// такой длины.
+// Теорема о неравенстве треугольника: каждая сторона треугольника
+// меньше суммы двух других сторон.
+
+int Promt(string message)
+{
+    Console.WriteLine(message);
+    string value = (Console.ReadLine());
+    int result = Convert.ToInt32(value);
+    return result;
+}
+
+bool CheckLengthSize(int a, int b, int c)
+{
+    return a < b + c && b < a + c && c < a + b;
+}
+
+int sizeA = Promt ("Введите первое число");
+int sizeB = Promt ("Введите второе число");
+int sizeC = Promt ("Введите третье число");
+
+bool triangleExist = CheckLengthSize(sizeA, sizeB, sizeC);
+// if(triangleExist) Console.Write("Треугольник существует");
+// else Console.Write("Треугольник не существует");
+
+Console.Write(triangleExist ? "Треугольник существует" : "Треугольник не существует");
---
+++ task42/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача 42: Напишите программу, которая будет преобразовывать
+// десятичное число в двоичное.
+// 45 -> 101101
+// 3 -> 11
+// 2 -> 10
+
+
+int Promt(string message)
+{
+    Console.WriteLine(message);
+    string value = (Console.ReadLine());
+    int result = Convert.ToInt32(value);
+    return result;
+}
+
+// string BinaryNum(int decimalNumber)
+// {
+//     var binaryNumber = string.Empty;
+//         while (decimalNumber > 0)
+//         {
+//             binaryNumber = (decimalNumber % 2) + binaryNumber;
+//             decimalNumber /= 2;
+//         }
+//     return binaryNumber;
+// }
+
+int BinaryNum(int num)
+{
+    int bin = 0;
+    while(num > 0)
+    {
+        bin = (num % 2) + bin;
+        num /= 2;
+        bin *= 10;
+    }
+}
+int number = Promt ("Введите число");
+Console.WriteLine(BinaryNum(number));
